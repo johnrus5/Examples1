6d2037406f3755033cb045792f3c78f137c3365a
Ivan B <johnrus@list.ru>
2022-11-14 22:59:32 +0300

    Zadach006 added

---
+++ Zadacha006/Program.cs
@@ -0,0 +1,12 @@
+﻿int i;
+Console.Write("Введите число: ");
+i = int.Parse(Console.ReadLine());
+
+if (i % 2 == 0)
+{
+    Console.WriteLine("Число четное - ДА");
+}
+else
+{
+    Console.WriteLine("Число четное - НЕТ");
+}
