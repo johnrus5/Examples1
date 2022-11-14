b9a125379dd70dfe54688c5609ba28137631ae29
Ivan B <johnrus@list.ru>
2022-11-14 07:31:21 +0300

    Zadacha002 added

---
+++ Zadacha002/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = new Random().Next(1, 20);
+Console.WriteLine(a);
+int b = new Random().Next(1, 20);
+Console.WriteLine(b);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int min = b;
+if (a < min) min = a;
+if (b < min) min = b;
+Console.Write("min = ");
+Console.Write(min);
