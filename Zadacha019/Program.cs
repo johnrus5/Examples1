9ed9e9f3179cee6add53a3fe331aff96758f1991
Ivan B <johnrus@list.ru>
2022-11-19 07:26:36 +0300

    Zadacha019 added

---
+++ Zadacha019/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - Палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - Не палиндром.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
+
