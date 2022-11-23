d750002d80c99e73fb108c7b96753304791264c4
Ivan B <johnrus@list.ru>
2022-11-23 11:36:23 +0300

    Zadacha025 added

---
+++ Zadacha025/Program.cs
@@ -0,0 +1,20 @@
+﻿int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+
+  Console.Write("Введите первое число : ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите второе число : ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Результат: " + exponentiation);
+
+
+
