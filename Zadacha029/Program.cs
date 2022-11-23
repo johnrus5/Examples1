2ed240d36577ee2c00fa61ec2cf4dc7e0f17afc4
Ivan B <johnrus@list.ru>
2022-11-23 23:23:34 +0300

    Zadacha029 added

---
+++ Zadacha029/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+//int size = new Random().Next(0, 100);
+int [] numbers = new int [numberN];
+for (int i=0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0,9);
+    Console.Write (numbers [i] + "," + " ");
+}
+    
+
