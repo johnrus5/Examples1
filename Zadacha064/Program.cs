333adb970c5a6b6edb31f137d0e2cf4ad9a4f92b
Ivan B <johnrus@list.ru>
2022-12-13 21:18:18 +0300

    Zadacha064 added

---
+++ Zadacha064/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = InputInt("Введите положительное число: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
